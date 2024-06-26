5df9184b7f4981b71f717750284ea2098956eaa2
Vincent Matthew Bautista <vincentmath12@gmail.com>
2024-06-26 15:16:40 +0800

    setup constants.dart

---
+++ pdax_flutter_exam/lib/config/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Color constants
+const primary = Color(0xFF79d862);
+const seconday = Color(0xFF09295c);
+const background = Color(0xFFffffff);
+
+// Text
+const mainText = Color(0xFFE6E9F0);
+const subText = Color(0xFF717171);
