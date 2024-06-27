ac517d7114a43be8c20c90146920a88171a25d73
Vincent Matthew Bautista <vincentmath12@gmail.com>
2024-06-28 00:23:34 +0800

    refactor person list main screen

--- pdax_flutter_exam/lib/feature/presentation/screens/person_list_screen.dart
+++ pdax_flutter_exam/lib/feature/presentation/screens/person_list_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pdax_flutter_exam/config/providers/dimension_provider/dimension_provider.dart';
 import 'package:pdax_flutter_exam/feature/presentation/components/desktop/desktop_person_list_screen.dart';
+import 'package:pdax_flutter_exam/feature/presentation/components/mobile/mobile_person_list_screen.dart';
 
 class PersonListScreen extends ConsumerWidget {
   const PersonListScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const DesktopPersonListScreen();
+    final dimension = ref.watch(dimensionControllerProvider);
+
+    if (dimension is DimensionMobile) {
+      return const MobilePersonListScreen();
+    }
+
+    if (dimension is DimensionDesktop) {
+      return const DesktopPersonListScreen();
+    }
+
+    return const MobilePersonListScreen();
   }
 }
