3f6037744ee3743c635493ee1220825002cb8b86
Vincent Matthew Bautista <vincentmath12@gmail.com>
2024-06-28 00:23:19 +0800

    refactor mobile view

--- pdax_flutter_exam/lib/feature/presentation/components/mobile/mobile_person_list_screen.dart
+++ pdax_flutter_exam/lib/feature/presentation/components/mobile/mobile_person_list_screen.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pdax_flutter_exam/config/constants.dart' as color_constants;
 import 'package:pdax_flutter_exam/feature/controller/person_list_controller/person_list_controller.dart';
+import 'package:pdax_flutter_exam/feature/controller/provider/person_list_provider.dart';
 import 'package:pdax_flutter_exam/feature/presentation/components/mobile/widget/mobile_person_list_widget.dart';
 
 class MobilePersonListScreen extends ConsumerStatefulWidget {
-  const MobilePersonListScreen({super.key});
+  const MobilePersonListScreen({
+    super.key,
+  });
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() =>
@@ -46,44 +49,59 @@ class _MobilePersonListState extends ConsumerState<MobilePersonListScreen> {
     super.dispose();
   }
 
-  // ==================================== UI ==================================== //
   @override
   Widget build(BuildContext context) {
-    //
-
+    // =================== UI =================== //
     return Scaffold(
       backgroundColor: color_constants.background,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 24,
-          ),
-          child: SingleChildScrollView(
-            controller: scrollController,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 16),
+      body: RefreshIndicator(
+        backgroundColor: color_constants.background,
+        color: color_constants.primary,
+        onRefresh: () async {
+          setState(() {
+            appendCount = 1;
+          });
 
-                // ================== Header ================== //
-                Text(
-                  'Person List',
-                  style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                ),
+          ref.invalidate(personListProvider);
+          ref.invalidate(isFirstFetchProvider);
 
-                const SizedBox(height: 16),
+          await ref
+              .read(personListControllerProvider.notifier)
+              .fetchPersonList();
+        },
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
+            ),
+            child: SingleChildScrollView(
+              controller: scrollController,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 16),
 
-                // ================== Person List Widget ================== //
-                MobilePersonListWidget(
-                  fetchCount: appendCount,
-                ),
+                  // ================== Header ================== //
+                  Text(
+                    'Person List',
+                    style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                  ),
 
-                const SizedBox(height: 16),
-              ],
+                  const SizedBox(height: 16),
+
+                  // ================== Person List Widget ================== //
+
+                  MobilePersonListWidget(
+                    fetchCount: appendCount,
+                  ),
+
+                  const SizedBox(height: 16),
+                ],
+              ),
             ),
           ),
         ),
--- pdax_flutter_exam/lib/feature/presentation/components/mobile/widget/mobile_person_list_widget.dart
+++ pdax_flutter_exam/lib/feature/presentation/components/mobile/widget/mobile_person_list_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pdax_flutter_exam/config/constants.dart' as color_constants;
 import 'package:pdax_flutter_exam/feature/controller/person_list_controller/person_list_controller.dart';
 import 'package:pdax_flutter_exam/feature/controller/provider/person_list_provider.dart';
+import 'package:pdax_flutter_exam/global/shimmer/shimmer.dart';
 
 class MobilePersonListWidget extends ConsumerWidget {
   const MobilePersonListWidget({
@@ -31,165 +32,141 @@ class MobilePersonListWidget extends ConsumerWidget {
     );
 
     // ================== Person Card/UI ================== //
-    return ListView.builder(
-      physics: const NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      itemCount: person.length + 1,
-      itemBuilder: (context, index) {
-        debugPrint('Test length = ${person.length}');
+    return !isFirstFetched
+        ? ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: person.length + 1,
+            itemBuilder: (context, index) {
+              debugPrint('Test length = ${person.length}');
 
-        if (index < person.length) {
-          final personData = person[index];
+              if (index < person.length) {
+                final personData = person[index];
 
-          return Padding(
-            padding: const EdgeInsets.only(
-              bottom: 16.0,
-            ),
-            child: Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 20,
-              ),
-              decoration: BoxDecoration(
-                color: color_constants.background,
-                border: Border.all(
-                  width: 4,
-                ),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              //
-              // ========== Container Details ========== //
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  // =================== Image =================== //
-                  Flexible(
-                    child: SizedBox(
-                      height: 48,
-                      width: 48,
-                      child: ClipOval(
-                        child: Image.network(
-                          personData.image!,
-                          errorBuilder: (context, error, stackTrace) {
-                            return Container(
-                              decoration: const BoxDecoration(
-                                color: Colors.black,
-                                shape: BoxShape.circle,
-                              ),
-                              child: Center(
-                                child: Text(
-                                  'Empty Image Link',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge
-                                      ?.copyWith(
-                                        color: color_constants.subText,
-                                        fontSize: 9,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
+                return Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 16.0,
                   ),
-
-                  const SizedBox(width: 16),
-
-                  Flexible(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 20,
+                    ),
+                    decoration: BoxDecoration(
+                      color: color_constants.background,
+                      border: Border.all(
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    //
+                    // ========== Container Details ========== //
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        // ========== Name ========== //
-                        Text(
-                          '${personData.lastname!}, ${personData.firstname!}',
-                          style:
-                              Theme.of(context).textTheme.labelLarge?.copyWith(
-                                    color: Colors.black,
-                                    fontSize: 16,
-                                  ),
+                        // =================== Image =================== //
+                        Flexible(
+                          child: SizedBox(
+                            height: 48,
+                            width: 48,
+                            child: ClipOval(
+                              child: Image.network(
+                                personData.image!,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Container(
+                                    decoration: const BoxDecoration(
+                                      color: color_constants.seconday,
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'Empty Image Link',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelLarge
+                                            ?.copyWith(
+                                              color: Colors.white,
+                                              fontSize: 9,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
                         ),
-                        //
-                        // ========== Email ========== //
-                        Text(
-                          personData.email!,
-                          style:
-                              Theme.of(context).textTheme.labelLarge?.copyWith(
-                                    color: Colors.black,
-                                    fontSize: 16,
-                                  ),
+
+                        const SizedBox(width: 16),
+
+                        Flexible(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // ========== Name ========== //
+                              Text(
+                                '${personData.lastname!}, ${personData.firstname!}',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelLarge
+                                    ?.copyWith(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                    ),
+                              ),
+                              //
+                              // ========== Email ========== //
+                              Text(
+                                personData.email!,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelLarge
+                                    ?.copyWith(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                    ),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
                   ),
-                ],
-              ),
-            ),
-          );
-        } else {
-          // =================== Load Data or No More Data =================== //
-          return fetchCount != 4
-              ? const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 24),
-                  child: Center(
-                    child: CircularProgressIndicator(
-                      color: color_constants.primary,
-                    ),
-                  ),
-                )
-              : Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 24,
-                  ),
-                  child: Center(
-                    child: Text(
-                      'No more data',
-                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                            fontSize: 14,
-                            color: color_constants.mainText,
-                          ),
-                    ),
-                  ),
                 );
-        }
-
-        // else {
-        //   return Center(
-        //     child: InkWell(
-        //       onTap: () async {
-        //         await ref
-        //             .read(personListControllerProvider.notifier)
-        //             .fetchPersonList();
-        //       },
-        //       child: Container(
-        //         width: 160,
-        //         height: 40,
-        //         decoration: BoxDecoration(
-        //           border: Border.all(
-        //             color: color_constants.primary,
-        //             width: 2,
-        //           ),
-        //           borderRadius: BorderRadius.circular(6),
-        //         ),
-        //         child: Center(
-        //           child: Text(
-        //             'Load More',
-        //             style:
-        //                 Theme.of(context).textTheme.headlineLarge?.copyWith(
-        //                       color: color_constants.primary,
-        //                       fontSize: 16,
-        //                     ),
-        //           ),
-        //         ),
-        //       ),
-        //     ),
-        //   );
-        // }
-      },
-    );
+              } else {
+                // =================== Load Data or No More Data =================== //
+                return fetchCount != 4
+                    ? const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 24),
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            color: color_constants.primary,
+                          ),
+                        ),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 24,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'No more data',
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge
+                                ?.copyWith(
+                                  fontSize: 14,
+                                  color: color_constants.mainText,
+                                ),
+                          ),
+                        ),
+                      );
+              }
+            },
+          )
+        : buildMobileLoadingSkeleton();
   }
 }
