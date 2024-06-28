1c5db23c35cb00e199b7d42fc9343b04a68faa6f
Vincent Matthew Bautista <vincentmath12@gmail.com>
2024-06-28 11:56:12 +0800

    add person details screen to router

--- pdax_flutter_exam/lib/router/router.dart
+++ pdax_flutter_exam/lib/router/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pdax_flutter_exam/feature/presentation/screens/person_details_screen.dart';
 import 'package:pdax_flutter_exam/feature/presentation/screens/person_list_screen.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -12,10 +13,18 @@ final navigatorKey = GlobalKey<NavigatorState>();
 List<GoRoute> get routes => [
       // ================= Profile List Screen ================= //
       GoRoute(
-        name: 'ProfileListScreen',
+        name: 'PersonListScreen',
         path: '/',
         redirect: (context, state) => '/',
         builder: (context, state) => const PersonListScreen(),
+      ),
+
+      // ================= Profile Details Screen ================= //
+      GoRoute(
+        name: 'PersonDetailsScreen',
+        path: '/details',
+        redirect: (context, state) => '/details',
+        builder: (context, state) => const PersonDetailsScreen(),
       ),
     ];
 
