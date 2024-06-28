a992a9cbaf7ed26e12aaf0917f3440c4408d019a
Vincent Matthew Bautista <vincentmath12@gmail.com>
2024-06-28 11:55:43 +0800

    refactor constants.dart

--- pdax_flutter_exam/lib/config/constants.dart
+++ pdax_flutter_exam/lib/config/constants.dart
@@ -6,5 +6,5 @@ const seconday = Color(0xFF09295c);
 const background = Color(0xFFffffff);
 
 // Text
-const mainText = Color(0xFFE6E9F0);
+const mainText = Colors.black;
 const subText = Color(0xFF717171);
